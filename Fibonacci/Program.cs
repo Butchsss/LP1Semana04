996f3655e19caf85e19804d25317ac3989d44a43
Butchsss <rodrigomarques23work@gmail.com>
2022-03-22 18:34:28 +0000

    Ex07 added

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(8));
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+            int prev = 0;
+            int next = 1;
+            for (int i = 1; i < n; i++)
+            {
+                int sum = prev + next;
+                prev = next;
+                next = sum;
+            }
+            return next;
+        }
+    }
+}
