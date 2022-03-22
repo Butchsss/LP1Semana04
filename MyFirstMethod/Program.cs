f0e610d3b85d0f8bc0cdfdd8274fa776232342dc
Butchsss <rodrigomarques23work@gmail.com>
2022-03-22 18:22:23 +0000

    Ex06 Added

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -14,6 +14,7 @@ namespace MyFirstMethod
             CountToN(2);
             CountToN(7);
             CountTo10();
+            CountToN(5,8);
         }
         /// <summary>
         /// Method who calls another method to count till 10
@@ -26,18 +27,33 @@ namespace MyFirstMethod
                 Console.WriteLine(i);
             }*/
         }
-/// <summary>
-/// Method that can be used to count till 'n'
-/// When this method is called, it'll count till the number was passed 
-/// in is parameter 
-/// </summary>
-/// <param name="n"></param>
+        /// <summary>
+        /// Method that can be used to count till 'n'
+        /// When this method is called, it'll count till the number was passed 
+        /// in is parameter 
+        /// </summary>
+        /// <param name="n"></param>
         private static void CountToN(int n)
         {
 
             for (int i = 1; i <= n; i++)
             {
 
+                Console.WriteLine(i);
+            }
+        }
+        /// <summary>
+        /// Method that can be used to count from n1 till n2
+        /// When this method is called, it'll count till the number was passed 
+        /// in is parameter, starting with the int given in the first parameter
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        private static async void CountToN(int n1, int n2)
+        {
+            for (int i = n1; i <= n2; i++)
+            {
                 Console.WriteLine(i);
             }
         }
