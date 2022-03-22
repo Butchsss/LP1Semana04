cccf53e50e053707c37bb3ee00347444cc58fcbd
Butchsss <rodrigomarques23work@gmail.com>
2022-03-22 18:17:52 +0000

    Ex05 Added

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,12 +4,20 @@ namespace MyFirstMethod
 {
     class Program
     {
+        /// <summary>
+        /// Main method 
+        /// Calls others methods
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             CountToN(2);
             CountToN(7);
             CountTo10();
         }
+        /// <summary>
+        /// Method who calls another method to count till 10
+        /// </summary>
         private static void CountTo10()
         {
             CountToN(10);
@@ -18,6 +26,12 @@ namespace MyFirstMethod
                 Console.WriteLine(i);
             }*/
         }
+/// <summary>
+/// Method that can be used to count till 'n'
+/// When this method is called, it'll count till the number was passed 
+/// in is parameter 
+/// </summary>
+/// <param name="n"></param>
         private static void CountToN(int n)
         {
 
